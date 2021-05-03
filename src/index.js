9927919c1b7f360fa4f00165f479d82215de21a4
Kwabena Agyeman <kagyeman@ymail.com>
2021-05-03 23:25:03 +0000

    pushing to gh pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
@@ -10,13 +10,13 @@ import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
-    <HashRouter>
+    <Router>
       <React.StrictMode>
         <PersistGate persistor={persistor}>
           <App />
         </PersistGate>
       </React.StrictMode>
-    </HashRouter>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
